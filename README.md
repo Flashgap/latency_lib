68b5607f3ba4bb13591c40dc7b58d13c6e3f64e9
Eliott Dumeix <eliott.dumeix@gmail.com>
2024-11-18 17:22:07 +0100

    requirements

---
+++ README.md
@@ -0,0 +1,3 @@
+# Latency helper library
+
+Contains some helper functions to work with BigQuery traces on Google Notebooks.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='latency_lib',
+    version='1.0.0',
+    url='https://github.com:Flashgap/latency_lib',
+    description='Latency helpers for Fruitz',
+    packages=find_packages(),
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
